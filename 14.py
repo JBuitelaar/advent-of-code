00e82689b958c1be19873b5651ea7363ad57aea1
Jacob Buitelaar <j.buitelaar@robeco.nl>
2023-12-15 22:27:03 +0100

    speed up

--- 14.py
+++ 14.py
@@ -1,6 +1,7 @@
 import time
+from bisect import bisect
 from aocd.models import Puzzle
-
+from collections import Counter
 puzzle = Puzzle(2023,14)
 data = puzzle.input_data
 # example = puzzle.examples[0]
@@ -10,59 +11,73 @@ data = puzzle.input_data
 lines = data.strip().split('\n')
 R = len(lines)
 C = len(lines[0])
-# location of the cubes. Add one at the beginning and end of each row and column
-cubes_by_row = [[0,C+1] for _ in range(R)]
-cubes_by_col = [[0,R+1] for _ in range(C)]
+assert R == C
+cubes = []
 balls = []
-for r,row in enumerate(lines,1):
-    row_balls = [0]
-    for c, val in enumerate(row,1):
+for r,row in enumerate(lines):
+    row_balls = [-1]
+    for c, val in enumerate(row):
         if val == "O":
             balls.append((r,c))
         elif val == "#":
-            cubes_by_col[c-1].append(r)
-            row_balls.append(c)
-    row_balls.append(R+1)
-    cubes_by_row[r-1]=tuple(row_balls)
+            cubes.append((r,c))
 
 balls = tuple(balls)
-cubes_by_col = [tuple(sorted(cubes)) for cubes in cubes_by_col]
 
-def roll(balls, cubes, reverse):
+
+def roll(balls, cubes):
     """roll the balls. 
     cubes is the location of the fixed rocks. 
-    an array of arrays, containing either the rownos for every column, or the columnnos for every row
-    reverse: roll up/left (False) or down/right (True) 
     """
-    new_balls = []
-    # We use col, r, c, but it could also be the other way around (row,c,r), depending on what cubes is
-    for col, cube_locs in enumerate(cubes,1):
-        for ix,r0 in enumerate(cube_locs[:-1]):
-            r1 = cube_locs[ix+1]
-            range_balls = sum(1 for (r,c) in balls if c==col and r0<r<r1)
-            for r in range(range_balls):
-                loc = r1-1-r if reverse else r0+1+r
-                new_balls.append((loc,col))
-    return tuple(new_balls)
+    res = []
+    count = [Counter() for _ in range(R)]
+    for (r,c) in balls:
+        j = bisect(cubes[c],r)-1
+        count[c][j]+=1
+        res.append((cubes[c][j]+count[c][j],c))
+    return tuple(sorted(res))
 
 def score(balls):
-    return sum(R+1-r for r,c in balls)
+    return sum(R-r for r,c in balls)
 
-def transpose(balls):
-    return tuple((c,r) for (r,c) in balls)
+def rotate(locations):
+    # rotate the grid 90 degrees clockwise
+    return tuple((c,R-1-r) for (r,c) in locations)
 
-def get_char(r,c,balls):
-    if (r+1,c+1) in balls:
+def bisectlist(values):
+    res = []
+    vals = sorted([-1]+values)
+    for ix,val in enumerate(vals):
+        # skip the rocks that are next to each other, because they don't matter
+        if ix==len(vals)-1 or val+1<vals[ix+1] and val<R-1:
+            res.append(val)
+    return tuple(res)
+
+def transform(locations):
+    # creates the relevant rock locations, so we can easily find where the balls will roll
+    res = [[] for _ in range(R)]
+    for (r,c) in locations:
+        res[c].append(r)
+    return tuple(bisectlist(r) for r in res)
+
+# location of the fixed rocks for NWSE (every step rotated 90 degrees clockwise)
+cube_by_dir = []
+for _ in range(4):
+    cube_by_dir.append(transform(cubes))
+    cubes = rotate(cubes)
+
+def get_char(r,c,balls,cubes_):
+    if (r,c) in balls:
         return "O"
-    if r+1 in cubes_by_col[c]:
+    if r in cubes_[c]:
         return "#"
     return "."
 
-def print_grid(balls):
+def print_grid(balls,cubes=cube_by_dir[0]):
     for r in range(R):
-        print("".join(get_char(r,c,balls) for c in range(C)))
+        print("".join(get_char(r,c,balls,cubes) for c in range(C)))
 
-new_balls = roll(balls, cubes_by_col, False)
+new_balls = roll(balls, cube_by_dir[0])
 # print_grid(new_balls)
 ans1=score(new_balls)
 print(f"{ans1=}")
@@ -73,19 +88,13 @@ balls_by_cycle=[balls]
 total_cycles=int(1e9)
 j=1
 while j<=total_cycles:
-    print(f"cycle {j}: t={time.time()-start:.2f}s")
-    for reverse in [False, True]:  # roll up/left (False) or down/right (True)
-        for is_row, cubes in enumerate([cubes_by_col, cubes_by_row]):  # up/down or left/right
-            if is_row:
-                # revert rows and columns, revert back after the calculation
-                balls = transpose(balls)
-            balls = roll(balls, cubes, reverse)
-            if is_row:
-                balls = transpose(balls)
+    # print(f"cycle {j}: t={time.time()-start:.2f}s")
+    for cubes in cube_by_dir:
+        balls = roll(balls, cubes)
+        balls = rotate(balls)
 
-    key = tuple(balls)
-    if key in cycle_cache:
-        start_cycle = cycle_cache[key]
+    if balls in cycle_cache:
+        start_cycle = cycle_cache[balls]
         cycle_length = j - start_cycle
         # now we actually have seen the answer already
         skip_cycles = (total_cycles - j ) // cycle_length
@@ -93,10 +102,11 @@ while j<=total_cycles:
     
         solution_ix = int(start_cycle + (total_cycles - j) % cycle_length)
         print(f"That means cycle {total_cycles} is equal to cycle {solution_ix}={total_cycles}-{skip_cycles}*{cycle_length}")
+        # We could also just skip skip_cycles*cycle_length steps, then we don't need to cache anything
         balls = balls_by_cycle[solution_ix]
         break
     else:
-        cycle_cache[key]=j
+        cycle_cache[balls]=j
         balls_by_cycle.append(balls)
     j+=1
 
