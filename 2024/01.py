a067dfbc8c64bcd894930696b2bee0cd0647b446
Jacob Buitelaar <j.buitelaar@robeco.nl>
2024-12-01 06:22:47 +0100

    day1

---
+++ 2024/01.py
@@ -0,0 +1,31 @@
+import time
+from collections import Counter
+from aocd.models import Puzzle
+import webbrowser
+chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path),preferred=True)
+
+puzzle = Puzzle(2024,1)
+data = puzzle.input_data
+# data = puzzle.examples[0].input_data
+# print(data)
+
+start_time = time.time()
+
+lines = data.strip().split('\n')
+
+list1,list2=[],[]
+
+for line in lines:
+    v1,v2=map(int,line.split())
+    list1.append(v1)
+    list2.append(v2)
+
+ans1=sum(abs(y-x) for x,y in zip( sorted(list1),sorted(list2)))
+timer = time.time() - start_time
+print(f"{ans1=}, {timer=:.2f}s")
+
+counts=Counter(list2)
+ans2=sum(v*counts.get(v,0) for v in list1)
+timer = time.time() - start_time
+print(f"{ans2=}, {timer=:.2f}s")
