5ddacf12d780badd89479d30c7e21bb0cd58a770
Jacob Buitelaar <j.buitelaar@robeco.nl>
2024-12-18 19:54:03 +0100

    generalize a bit

--- 2024/17.py
+++ 2024/17.py
@@ -25,7 +25,7 @@ def run_program(registers, instructions):
         assert operand < 7
         return regs[operand - 4] if operand > 3 else operand
 
-    output = []
+    res = []
     pointer = 0
     while pointer < len(instructions):
         opcode = instructions[pointer]
@@ -43,63 +43,43 @@ def run_program(registers, instructions):
             case 4:
                 regs[1] = regs[1] ^ regs[2]
             case 5:
-                output.append(combo(operand) % 8)
+                res.append(combo(operand) % 8)
             case 0 | 6 | 7:
                 # these opcodes have the same operation, but just write to a different registry
                 regs[opcode % 5] = regs[0] // 2 ** combo(operand)
             case _:
                 raise ValueError(opcode)
         pointer += 2
-    return tuple(output)
+    return tuple(res)
 
 
-first_output = run_program(registers, program)
-ans1 = ",".join([str(v) for v in first_output])
+res = run_program(registers, program)
+ans1 = ",".join([str(v) for v in res])
 
 timer = time.time() - start_time
 print(f"{ans1=}, {timer=:.2f}s")
 
-### PART 2 ###
 
+""" ### PART 2 ###
+Looking at the program, it basically does this:
 
-# Looking at the program, it's basically this:
-# We can see that the initial value of B and C don't matter, because they get set at the beginning.
-def run_program2(A):
     output = []
     while A:
         B = A % 8
         B = B ^ 1
-        C = A // 2**B % 8  # added %8
+        C = A // 2**B
         A //= 8
         B = B ^ C
         B = B ^ 6
         out = B % 8
         output.append(out)
-    return tuple(output)
 
-
-# verify that this is true:
-assert run_program2(registers[0]) == first_output
-
-"""
-Now we see that in every iteration A is divided by 8 and nothing else happens to it.
-Since all operations are bitwise, if we write A in binary, we basically chop off the last 3 bits.
-So at the last iteration before 0, the value must be between 1 and 7. 
-We can try all these 7 numbers to see which one produces the LAST output.
-That way we find the first 3 bits of A.
-Now going one step back, we have a 6-bit number. We already know the first 3, so again we have
-to only check the 8 options for the last 3 bits to produce the 2nd last output.
-This we can repeat until we have found the value for A that produces all outputs.
-
-Note that there can be multiple solutions in each step, but not all might work later on. 
-So we need to keep track of all possible values for A.
-
-For this we really only need to know the first output that is produced by a number. 
-That simplifies the program even further:
+From this we can see that the inputs B and C don't matter, because they get overwritten at the beginning of the loop.
+We can rewrite this to:
 """
 
 
-def first_output(A):
+def output(A):
     B = A % 8
     B = B ^ 1
     C = A // 2**B
@@ -108,23 +88,48 @@ def first_output(A):
     return B % 8
 
 
+def run_program2(A):
+    res = []
+    while A:
+        res.append(output(A))
+        A //= 8
+    return tuple(res)
+
+
+# verify that this is true:
+assert run_program2(registers[0]) == res
+
+"""
+If we would write A in base 8, we chop off the last digit in every iteration (A//=8).
+So we can work backwards. The first (base-8) digit of A should produce the last output.
+I don't know how to invert the output function, but we can simply try every value 1-7
+(not 0, because the program would have stopped in the previous iteraton).
+
+For the second last output, we need a 2-digit number, where we already know the first one from previous step.
+Again we can try every value for this digit, this time 0 is also allowed.
+
+Note in every step there can be multiple solutions, but not every solution might be feasible in the next steps.
+So we keep track of all options.
+We end up with several potential values for A and we take the minimum.
+"""
+
+base = 8
+
 candidates = [0]
 for ix, val in enumerate(reversed(program)):
-    range_start = (
-        ix == 0
-    )  # first iteration can not be 0, as that would exit the program
+    range_start = ix == 0  # first iteration can not be 0
     new_candidates = []
     for A in candidates:
-        A *= 8
-        for new_bits in range(range_start, 8):
+        A *= base
+        for new_bits in range(range_start, base):
             v = A + new_bits
-            if first_output(v) == val:
+            if output(v) == val:
                 new_candidates.append(v)
     assert len(new_candidates)
     candidates = new_candidates
 
 # print(f"{candidates=}")
-# All candidates create the same output:
+# Check that all candidates create the same output:
 assert all(run_program2(c) == program for c in candidates)
 
 ans2 = min(candidates)
